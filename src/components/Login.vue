6d5d704dd9d563bdb822d47a308559843f897ec7
Anmizi <1845513904@qq.com>
2021-11-03 17:36:16 +0800

    :beer: 添加登录界面

---
+++ src/components/Login.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="">
+      </div>
+      <!-- 登录表单区域 -->
+      <el-form class="login_form">
+        <!-- 用户名 -->
+        <el-form-item>
+          <el-input prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item>
+          <el-input prefix-icon="iconfont icon-3702mima"></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style lang="less" scoped>
+.login_container{
+  height: 100%;
+  background-color: #2b4b6b;
+  .login_box{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    width: 450px;
+    height: 300px;
+    border-radius: 8px;
+    background-color: #fff;
+    .avatar_box{
+      overflow: hidden;
+      margin: -50px auto 0 auto;
+      padding: 10px;
+      width: 100px;
+      height: 100px;
+      border: 1px solid #ccc;
+      border-radius: 50%;
+      box-shadow: 0 0 10px #ddd;
+      img{
+        width: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+      }
+    }
+    .login_form{
+      position: absolute;
+      width: 100%;
+      padding: 0 20px;
+      box-sizing: border-box;
+      bottom: 0;
+      .btns{
+        display: flex;
+        justify-content: flex-end;
+      }
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
 
 Vue.config.productionTip = false
 
