9b670de1755638e531082cbd4e03139742b846e0
Anmizi <1845513904@qq.com>
2021-11-04 11:14:06 +0800

    :art: 优化代码结构

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message
+} from 'element-ui'
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
