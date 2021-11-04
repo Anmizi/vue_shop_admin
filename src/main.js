d4a80e17255eb46ce5009602fcc486c497918b74
Anmizi <1845513904@qq.com>
2021-11-04 19:33:26 +0800

    axios拦截器添加token验证

--- src/main.js
+++ src/main.js
@@ -10,6 +10,16 @@ import './assets/css/global.css'
 import axios from 'axios'
 // 配置请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 为发送请求添加请求验证头部Authorization
+  // 保证有获取数据的权限
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error)
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
