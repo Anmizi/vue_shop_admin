c8a536f0b443681287e95f91116ab19a15a62fe5
Anmizi <1845513904@qq.com>
2021-11-04 18:53:18 +0800

    :beer: 主要头部区域

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>Home</div>
+  <el-container class="home-container">
+  <el-header>
+    <div class="home-logo">
+      <img src="../assets/home-logo.png" alt="">
+    </div>
+    <el-button type="info" class="logout-btn">退出登录</el-button>
+  </el-header>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
@@ -9,4 +20,24 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.home-container{
+  height: 100%;
+  .el-header{
+    padding-left: 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .home-logo{
+      height: 100%;
+      img{
+        height: 100%;
+      }
+    }
+    }
+    background: #eeeeee;
+  .el-aside{
+    background: #304156;
+  }
+}
+
 </style>
