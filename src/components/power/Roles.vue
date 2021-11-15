21e08287cefd96633925797f497bc4bfcb6c5951
Anmizi <1845513904@qq.com>
2021-11-15 10:56:13 +0800

    :sparkles: 添加权限管理角色列表组件

---
+++ src/components/power/Roles.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <el-button type="primary">添加角色</el-button>
+      <el-table :data="rolesList" stripe border>
+        <el-table-column width="60px" type="expand">
+          <template v-slot="{ row }">
+            <el-row v-for="(item,i1) in row.children" :key="item.id" :class="['bdbottom', i1 === 0 ? 'bdtop' : '']">
+              <!-- 渲染一级权限 -->
+              <el-col :span="5">
+                <el-tag>{{item.authName}}</el-tag>
+              </el-col>
+              <!-- 渲染二三级权限 -->
+              <el-col :span="19"></el-col>
+            </el-row>
+          </template>
+        </el-table-column>
+        <el-table-column type="index" label="#"> </el-table-column>
+        <el-table-column prop="roleName" label="角色名称"> </el-table-column>
+        <el-table-column prop="roleDesc" label="角色描述"> </el-table-column>
+        <el-table-column label="操作">
+          <template>
+            <!-- 编辑按钮 -->
+            <el-button type="primary" icon="el-icon-edit" size="mini"
+              >编辑</el-button
+            >
+            <!--删除按钮 -->
+            <el-button type="danger" icon="el-icon-delete" size="mini"
+              >删除</el-button
+            >
+            <!-- 分配权限按钮 -->
+            <el-tooltip
+              class="item"
+              effect="light"
+              content="分配角色"
+              placement="top"
+              :enterable="false"
+            >
+              <el-button type="warning" icon="el-icon-setting" size="mini"
+                >分配权限</el-button
+              >
+            </el-tooltip>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Roles',
+  data () {
+    return {
+      rolesList: []
+    }
+  },
+  created () {
+    this.getRolesList()
+  },
+  methods: {
+    async getRolesList () {
+      const { data: res } = await this.$http.get('/roles')
+      console.log(res)
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取角色列表失败')
+      }
+      this.$message.success('获取角色列表成功')
+      this.rolesList = res.data
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.el-card{
+  margin-bottom: 100px;
+}
+.el-row{
+  margin: 0 20px;
+}
+.el-tag{
+  margin: 10px;
+}
+.bdtop{
+  border-top: 1px solid #eee;
+}
+.bdbottom{
+  border-bottom: 1px solid #eee;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Home from '../components/Home'
 import Welcome from '../components/Welcome'
 import Users from '../components/user/Users'
 import Rights from '../components/power/Rights'
+import Roles from '../components/power/Roles'
 Vue.use(VueRouter)
 
 const routes = [
@@ -17,7 +18,8 @@ const routes = [
     children: [
       { path: '/welcome', component: Welcome },
       { path: '/users', component: Users },
-      { path: '/rights', component: Rights }
+      { path: '/rights', component: Rights },
+      { path: '/roles', component: Roles }
     ]
   }
 ]
